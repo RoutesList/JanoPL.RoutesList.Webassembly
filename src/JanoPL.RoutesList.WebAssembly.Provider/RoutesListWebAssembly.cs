daf34f6959a383cf8c18f56fbb197d9e87530b21
JanoPL <xjanusz4@gmail.com>
2024-05-02 19:08:10 +0200

    [feat] add webassembly service provider

---
+++ src/JanoPL.RoutesList.WebAssembly.Provider/RoutesListWebAssembly.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace JanoPL.RoutesList.WebAssembly.Provider;
+
+public static class RoutesListWebAssembly
+{
+    public static IServiceCollection AddRoutesList(
+        this IServiceCollection services)
+    {
+        return services;
+    }
+}
